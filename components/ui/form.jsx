1341bb7b96c9edc096c6aa14d9c7eba32d473e57
Alex-parabol <68709781+Alex-parabol@users.noreply.github.com>
2021-12-01 19:27:42 +0100

    ready for custom hooks

---
+++ components/ui/form.jsx
@@ -0,0 +1,46 @@
+import styled from "@emotion/styled";
+
+export const Form = styled.form`
+   max-width: 600px;
+   width: 95%;
+   margin: 5rem auto 0 auto;
+`
+
+export const Field = styled.div`
+    display: flex;
+    align-items: center;
+    margin-bottom: 2rem;
+    font-family: 'nunito';
+
+    label {
+        flex: 0 0 150px;
+        font-size: 1.5rem;
+    }
+
+    input {
+        flex: 1;
+        padding: 0.5rem;
+        border: 1px solid var(--gris3);
+        border-radius: 5px;
+    }
+`
+
+export const InputSubmit = styled.input`
+  background-color: var(--naranja);
+  width: 50%;
+  padding: 1rem;
+  margin-left: 25%;
+  text-align: center;
+  color: #fff;
+  text-transform: uppercase;
+  border: none;
+  border-radius: 5px;
+  font-family:'Nunito' ;
+  font-weight: 700;
+  
+
+  &:hover {
+      cursor: pointer;
+      opacity: 80%;
+  }
+`
--- pages/create-account.jsx
+++ pages/create-account.jsx
@@ -1,13 +1,57 @@
 import React from 'react'
 import Layout from '../components/layout/Layout'
+import { css } from '@emotion/react';
+import {Form, Field, InputSubmit} from '../components/ui/form'
 
 export default function CreateAccount() {
     return (
       <div>
         <Layout>
-          <h1>Create Account</h1>
+          <>
+          <h1
+            css={css`
+              text-align: center;
+              margin-top: 5rem;
+            `}
+          >Create Account</h1>
+            <Form>
+              <Field>
+                <label htmlFor="name">Name</label>
+                  <input 
+                    type="text"
+                    id='name'
+                    placeholder='Your name'
+                    name='name'
+                  />
+              </Field>
+
+              <Field>
+                <label htmlFor="email">Email</label>
+                  <input 
+                    type="email"
+                    id='email'
+                    placeholder='Your email'
+                    name='email'
+                  />
+              </Field>
+
+              <Field>
+                <label htmlFor="password">Password</label>
+                  <input 
+                    type="password"
+                    id='password'
+                    placeholder='Your Password'
+                    name='password'
+                  />
+              </Field>
+
+              <InputSubmit 
+                type="submit"
+                value='Create Account'
+              />
+            </Form>
+          </>
          </Layout>
-  
       </div>
       
     )
