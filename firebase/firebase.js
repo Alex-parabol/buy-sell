acc27bbf8af27cbb841ff5a4819336c4a30a6b79
Alex-parabol <68709781+Alex-parabol@users.noreply.github.com>
2021-12-05 21:18:46 +0100

    firebase config

---
+++ firebase/firebase.js
@@ -0,0 +1,13 @@
+import app from 'firebase/compat/app';
+
+import firebaseConfig from './config';
+
+class Firebase {
+    constructor(){
+        app.initializeApp(firebaseConfig)
+    }
+}
+
+const firebase = new Firebase();
+
+export default firebase;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import App from 'next/app'
+import firebase, { FirebaseContext} from '../firebase'
+
+function MyApp(props) {
+
+  const {Component, pageProps} = props;
+  return (
+    <FirebaseContext.Provider
+    value={{
+      firebase
+    }}>
+        <Component {...pageProps}/>
+    </FirebaseContext.Provider>
+  )
 }
 
 export default MyApp
